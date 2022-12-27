7a0da1390eee73d4a9b0993e12fa4cd47e9e542a
SeanQuinn <Seanquinn917@gmail.com>
2022-12-27 14:43:41 -0500

    commit tree 1c5225c98d9ec7dcc04331198793e74eab988378

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = function(num1, num2){
+    return num1/num2
+}
+// num1 = 2000
+// num2 = 100;
+
+// function square (parameter) {
+//  return parameter ** 2;
+// }
+
+// function add (parameter1, parameter2) {
+//     return parameter1 + parameter2;
+// }
+
+const add = (a, b) => a + b;
+
+const square = parameter => parameter ** 2;
